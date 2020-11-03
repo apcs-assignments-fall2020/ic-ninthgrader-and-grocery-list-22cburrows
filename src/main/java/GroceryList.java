8a9702ccb4b7876fd6dd09ae209131e97bda38b6
22cburrows <22cburrows@athenian.org>
2020-11-03 05:16:42 +0000

    not sure why this is not yet running

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,35 @@
+import java.util.Arrays;  
 public class GroceryList {
-    
-}
+
+    private String[] arr;
+
+    public GroceryList() {
+        // our grocery list will have a max size of 10
+        this.arr = new String[10];
+    }
+
+    public void add(final String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+        }
+    }
+
+    public void remove(final String item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                for (int j = (arr.length - i); j < (arr.length - i); j++) {
+                    arr[i] = arr[j];
+                    break;
+                }
+            }
+        }
+    }
+
+    public String toString() {
+        String str = "Grocery List: " + Arrays.toString(arr);
+        return str;
+    }
+}   
